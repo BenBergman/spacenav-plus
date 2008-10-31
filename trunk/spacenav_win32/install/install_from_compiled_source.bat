a9dc849ef3c4aed3d8eb9610f5d6427d6d40d3fe
christian_bay <christian_bay@9acbd7d0-4e34-0410-9de4-80efa1d9a8fd>
2008-10-31 18:01:55 +0000

    Revert initial import..
    git-svn-id: https://spacenav.svn.sourceforge.net/svnroot/spacenav@53 9acbd7d0-4e34-0410-9de4-80efa1d9a8fd

--- trunk/spacenav_win32/install/install_from_compiled_source.bat
+++
@@ -1,3 +0,0 @@
-%WINDIR%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe ..\bin\Debug\hidlibrary.dll
-%WINDIR%\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe /codebase ..\bin\Debug\tdxinput.dll /tlb:..\bin\Debug\TDxInput.tlb
-regedit progid_device_register.reg
