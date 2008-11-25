a635cbc6e83935c8451ae269bb59d1a33e3af5ee
christian_bay <christian_bay@9acbd7d0-4e34-0410-9de4-80efa1d9a8fd>
2008-11-25 10:00:56 +0000

    Copyright notice added
    git-svn-id: https://spacenav.svn.sourceforge.net/svnroot/spacenav@61 9acbd7d0-4e34-0410-9de4-80efa1d9a8fd

--- trunk/spacenav_win32/HIDLibrary/HidControl.cs
+++ trunk/spacenav_win32/HIDLibrary/HidControl.cs
@@ -1,3 +1,24 @@
+/*
+   HidLibrary.Net   http://sourceforge.net/projects/hidlibrary/
+ 
+   Copyright (C)    Roman Reichel 2006
+					Bauhaus University of Weimar
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see http://www.gnu.org/licenses .
+
+*/
+
 using System;
 using System.Collections;
 using System.Windows.Forms;
